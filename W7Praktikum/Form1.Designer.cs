3f65b355087d2b5a57e19429666eab10f2f85ac0
FahrizalDR <frinaldi@student.ciputra.ac.id>
2022-04-04 19:17:18 +0700

    Add project files.

---
+++ W7Praktikum/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿
+namespace W7Praktikum
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelMasukkanInput = new System.Windows.Forms.Label();
+            this.labelMasukkanHuruf = new System.Windows.Forms.Label();
+            this.labelHasil = new System.Windows.Forms.Label();
+            this.labelOutput = new System.Windows.Forms.Label();
+            this.txtBoxInputKalimat = new System.Windows.Forms.TextBox();
+            this.txtBoxInputHuruf = new System.Windows.Forms.TextBox();
+            this.txtBoxMenjadi = new System.Windows.Forms.TextBox();
+            this.labelMenjadi = new System.Windows.Forms.Label();
+            this.buttonKonversi = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelMasukkanInput
+            // 
+            this.labelMasukkanInput.AutoSize = true;
+            this.labelMasukkanInput.Location = new System.Drawing.Point(56, 57);
+            this.labelMasukkanInput.Name = "labelMasukkanInput";
+            this.labelMasukkanInput.Size = new System.Drawing.Size(133, 20);
+            this.labelMasukkanInput.TabIndex = 0;
+            this.labelMasukkanInput.Text = "Masukkan Kalimat ";
+            // 
+            // labelMasukkanHuruf
+            // 
+            this.labelMasukkanHuruf.AutoSize = true;
+            this.labelMasukkanHuruf.Location = new System.Drawing.Point(57, 110);
+            this.labelMasukkanHuruf.Name = "labelMasukkanHuruf";
+            this.labelMasukkanHuruf.Size = new System.Drawing.Size(115, 20);
+            this.labelMasukkanHuruf.TabIndex = 1;
+            this.labelMasukkanHuruf.Text = "Masukkan Huruf";
+            // 
+            // labelHasil
+            // 
+            this.labelHasil.AutoSize = true;
+            this.labelHasil.Location = new System.Drawing.Point(66, 224);
+            this.labelHasil.Name = "labelHasil";
+            this.labelHasil.Size = new System.Drawing.Size(49, 20);
+            this.labelHasil.TabIndex = 2;
+            this.labelHasil.Text = "Hasil :";
+            // 
+            // labelOutput
+            // 
+            this.labelOutput.AutoSize = true;
+            this.labelOutput.Location = new System.Drawing.Point(195, 224);
+            this.labelOutput.Name = "labelOutput";
+            this.labelOutput.Size = new System.Drawing.Size(0, 20);
+            this.labelOutput.TabIndex = 3;
+            // 
+            // txtBoxInputKalimat
+            // 
+            this.txtBoxInputKalimat.Location = new System.Drawing.Point(195, 57);
+            this.txtBoxInputKalimat.Name = "txtBoxInputKalimat";
+            this.txtBoxInputKalimat.Size = new System.Drawing.Size(310, 27);
+            this.txtBoxInputKalimat.TabIndex = 4;
+            // 
+            // txtBoxInputHuruf
+            // 
+            this.txtBoxInputHuruf.Location = new System.Drawing.Point(195, 110);
+            this.txtBoxInputHuruf.Name = "txtBoxInputHuruf";
+            this.txtBoxInputHuruf.Size = new System.Drawing.Size(102, 27);
+            this.txtBoxInputHuruf.TabIndex = 5;
+            // 
+            // txtBoxMenjadi
+            // 
+            this.txtBoxMenjadi.Location = new System.Drawing.Point(403, 110);
+            this.txtBoxMenjadi.Name = "txtBoxMenjadi";
+            this.txtBoxMenjadi.Size = new System.Drawing.Size(102, 27);
+            this.txtBoxMenjadi.TabIndex = 6;
+            // 
+            // labelMenjadi
+            // 
+            this.labelMenjadi.AutoSize = true;
+            this.labelMenjadi.Location = new System.Drawing.Point(334, 113);
+            this.labelMenjadi.Name = "labelMenjadi";
+            this.labelMenjadi.Size = new System.Drawing.Size(63, 20);
+            this.labelMenjadi.TabIndex = 7;
+            this.labelMenjadi.Text = "Menjadi";
+            // 
+            // buttonKonversi
+            // 
+            this.buttonKonversi.Location = new System.Drawing.Point(195, 162);
+            this.buttonKonversi.Name = "buttonKonversi";
+            this.buttonKonversi.Size = new System.Drawing.Size(310, 29);
+            this.buttonKonversi.TabIndex = 8;
+            this.buttonKonversi.Text = "Konversi";
+            this.buttonKonversi.UseVisualStyleBackColor = true;
+            this.buttonKonversi.Click += new System.EventHandler(this.buttonKonversi_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(676, 358);
+            this.Controls.Add(this.buttonKonversi);
+            this.Controls.Add(this.labelMenjadi);
+            this.Controls.Add(this.txtBoxMenjadi);
+            this.Controls.Add(this.txtBoxInputHuruf);
+            this.Controls.Add(this.txtBoxInputKalimat);
+            this.Controls.Add(this.labelOutput);
+            this.Controls.Add(this.labelHasil);
+            this.Controls.Add(this.labelMasukkanHuruf);
+            this.Controls.Add(this.labelMasukkanInput);
+            this.Name = "Form1";
+            this.Text = "Pembuat Quiz Panda";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelMasukkanInput;
+        private System.Windows.Forms.Label labelMasukkanHuruf;
+        private System.Windows.Forms.Label labelHasil;
+        private System.Windows.Forms.Label labelOutput;
+        private System.Windows.Forms.TextBox txtBoxInputKalimat;
+        private System.Windows.Forms.TextBox txtBoxInputHuruf;
+        private System.Windows.Forms.TextBox txtBoxMenjadi;
+        private System.Windows.Forms.Label labelMenjadi;
+        private System.Windows.Forms.Button buttonKonversi;
+    }
+}
+
---
+++ W7Praktikum/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace W7Praktikum
+{
+    public partial class Form1 : Form
+    {
+        public string InputHuruf = "";
+        public string Menjadi = "";
+        public string Hasil = "";
+        public string[] IndexHuruf = new string[30];
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonKonversi_Click(object sender, EventArgs e)
+        {
+            InputHuruf = txtBoxInputHuruf.Text;
+            Menjadi = txtBoxMenjadi.Text;
+            if (txtBoxMenjadi.Text.ToUpper() == "A")
+                txtBoxInputHuruf.Text = IndexHuruf[0];
+            else if (txtBoxMenjadi.Text.ToUpper() == "B")
+                txtBoxInputHuruf.Text = IndexHuruf[1];
+            else if (txtBoxMenjadi.Text.ToUpper() == "C")
+                txtBoxInputHuruf.Text = IndexHuruf[2];
+            else if (txtBoxMenjadi.Text.ToUpper() == "D")
+                txtBoxInputHuruf.Text = IndexHuruf[3];
+            else if (txtBoxMenjadi.Text.ToUpper() == "E")
+                txtBoxInputHuruf.Text = IndexHuruf[4];
+            else if (txtBoxMenjadi.Text.ToUpper() == "F")
+                txtBoxInputHuruf.Text = IndexHuruf[5];
+            else if (txtBoxMenjadi.Text.ToUpper() == "G")
+                txtBoxInputHuruf.Text = IndexHuruf[6];
+            else if (txtBoxMenjadi.Text.ToUpper() == "H")
+                txtBoxInputHuruf.Text = IndexHuruf[7];
+            else if (txtBoxMenjadi.Text.ToUpper() == "I")
+                txtBoxInputHuruf.Text = IndexHuruf[8];
+            else if (txtBoxMenjadi.Text.ToUpper() == "J")
+                txtBoxInputHuruf.Text = IndexHuruf[9];
+            else if (txtBoxMenjadi.Text.ToUpper() == "K")
+                txtBoxInputHuruf.Text = IndexHuruf[10];
+            else if (txtBoxMenjadi.Text.ToUpper() == "L")
+                txtBoxInputHuruf.Text = IndexHuruf[11];
+            else if (txtBoxMenjadi.Text.ToUpper() == "M")
+                txtBoxInputHuruf.Text = IndexHuruf[12];
+            else if (txtBoxMenjadi.Text.ToUpper() == "N")
+                txtBoxInputHuruf.Text = IndexHuruf[13];
+            else if (txtBoxMenjadi.Text.ToUpper() == "O")
+                txtBoxInputHuruf.Text = IndexHuruf[14];
+            else if (txtBoxMenjadi.Text.ToUpper() == "P")
+                txtBoxInputHuruf.Text = IndexHuruf[15];
+            else if (txtBoxMenjadi.Text.ToUpper() == "Q")
+                txtBoxInputHuruf.Text = IndexHuruf[16];
+            else if (txtBoxMenjadi.Text.ToUpper() == "R")
+                txtBoxInputHuruf.Text = IndexHuruf[17];
+            else if (txtBoxMenjadi.Text.ToUpper() == "S")
+                txtBoxInputHuruf.Text = IndexHuruf[18];
+            else if (txtBoxMenjadi.Text.ToUpper() == "T")
+                txtBoxInputHuruf.Text = IndexHuruf[19];
+            else if (txtBoxMenjadi.Text.ToUpper() == "U")
+                txtBoxInputHuruf.Text = IndexHuruf[20];
+            else if (txtBoxMenjadi.Text.ToUpper() == "V")
+                txtBoxInputHuruf.Text = IndexHuruf[21];
+            else if (txtBoxMenjadi.Text.ToUpper() == "W")
+                txtBoxInputHuruf.Text = IndexHuruf[22];
+            else if (txtBoxMenjadi.Text.ToUpper() == "X")
+                txtBoxInputHuruf.Text = IndexHuruf[23];
+            else if (txtBoxMenjadi.Text.ToUpper() == "Y")
+                txtBoxInputHuruf.Text = IndexHuruf[24];
+            else if (txtBoxMenjadi.Text.ToUpper() == "Z")
+                txtBoxInputHuruf.Text = IndexHuruf[25];            
+
+            Hasil = txtBoxInputKalimat.Text.Replace(InputHuruf, Menjadi).ToUpper();
+            labelOutput.Text = Hasil;
+            txtBoxInputHuruf.Text = "";
+            txtBoxMenjadi.Text = "";
+            txtBoxInputKalimat.Focus();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {            
+            IndexHuruf[0] = "A";
+            IndexHuruf[1] = "B";
+            IndexHuruf[2] = "C";
+            IndexHuruf[3] = "D";
+            IndexHuruf[4] = "E";
+            IndexHuruf[5] = "F";
+            IndexHuruf[6] = "G";
+            IndexHuruf[7] = "H";
+            IndexHuruf[8] = "I";
+            IndexHuruf[9] = "J";
+            IndexHuruf[10] = "K";
+            IndexHuruf[11] = "L";
+            IndexHuruf[12] = "M";
+            IndexHuruf[13] = "N";
+            IndexHuruf[14] = "O";
+            IndexHuruf[15] = "P";
+            IndexHuruf[16] = "Q";
+            IndexHuruf[17] = "R";
+            IndexHuruf[18] = "S";
+            IndexHuruf[19] = "T";
+            IndexHuruf[20] = "U";
+            IndexHuruf[21] = "V";
+            IndexHuruf[22] = "W";
+            IndexHuruf[23] = "X";
+            IndexHuruf[24] = "Y";
+            IndexHuruf[25] = "Z";                       
+        }
+    }
+}
+
